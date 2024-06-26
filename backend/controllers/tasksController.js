b0ab2cd657b7048470985cef3cd08a34df577505
YOUSSEF <imdevyoussef@gmail.com>
2024-06-26 21:38:33 +0300

    tasks controllers - validation added

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -1,4 +1,5 @@
 import Task from "../models/taskModel.js";
+import Yup from "yup";
 
 export const getMyTasks = async (req, res) => {
   try {
@@ -16,7 +17,26 @@ export const createTask = async (req, res) => {
       user,
       body: { name, description, status },
     } = req;
-    // TODO: VALIDATE INPUTS
+
+    await Yup.string()
+      .label("Task name")
+      .required()
+      .min(3)
+      .max(30)
+      .validate(name);
+    await Yup.string()
+      .label("Description")
+      .required()
+      .min(10)
+      .max(300)
+      .validate(description);
+
+    const task = await Task.findOne({ userId: user._id, name });
+    if (task)
+      throw new Error(
+        `This task already listed, and it's current status is: ${task.status}`
+      );
+
     const newTask = await Task.create({
       name,
       description,
@@ -35,8 +55,31 @@ export const createTask = async (req, res) => {
 
 export const updateTask = async (req, res) => {
   try {
-    const {task, body:{name, description, status}} = req;
-    // TODO: VALIDATE INPUTS
+    const {
+      user,
+      task,
+      body: { name, description, status },
+    } = req;
+
+    await Yup.string()
+      .label("Task name")
+      .required()
+      .min(3)
+      .max(30)
+      .validate(name);
+    await Yup.string()
+      .label("Description")
+      .required()
+      .min(10)
+      .max(300)
+      .validate(description);
+
+    const tasksWithSameName = await Task.find({ userId: user._id, name });
+    if (tasksWithSameName.length > 1)
+      throw new Error(
+        `This task already listed, and it's current status is: ${task.status}`
+      );
+
     if (name && name !== task.name) await task.$set("name", name);
     if (description && description !== task.description)
       await task.$set("description", description);
@@ -56,12 +99,15 @@ export const updateTask = async (req, res) => {
 
 export const deleteTask = async (req, res) => {
   try {
-    const { task, params: {taskId} } = req;
+    const {
+      task,
+      params: { taskId },
+    } = req;
 
-    await Task.deleteOne({_id: taskId})
+    await Task.deleteOne({ _id: taskId });
 
-    res.status(201).json({success: true, message: "Successfully deleted."})
+    res.status(201).json({ success: true, message: "Successfully deleted." });
   } catch (error) {
-    res.status(400).json({success: false, message: error.message})
+    res.status(400).json({ success: false, message: error.message });
   }
 };
--- backend/middlewares/taskMiddleware.js
+++ backend/middlewares/taskMiddleware.js
@@ -1,11 +1,12 @@
 import Task from "../models/taskModel.js";
 
 export const checkTaskAndUserAuth = async(req, res, next) => {
-    const {user, params: {taskId}} = req
+    try {
+      const {user, params: {taskId}} = req
     const task = await Task.findById(taskId);
     if (!task) throw new Error("Task not found.");
 
-    if (task._id.toString() !== user._id.toString()) {
+    if (task.userId.toString() !== user._id.toString()) {
       res.status(403);
       throw new Error("You're not permitted to controll on this task.");
     }
@@ -13,4 +14,7 @@ export const checkTaskAndUserAuth = async(req, res, next) => {
     req.task = task
 
     next()
+    } catch (error) {
+      res.status(403).json({success: false, message: error.message})
+    }
 }
