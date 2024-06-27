ebad1305a36a2fed3422299145ee628d7262d185
YOUSSEF <imdevyoussef@gmail.com>
2024-06-27 12:09:23 +0300

    frontend: protected routes

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import { Task } from "../components";
-import { useSelector } from "react-redux";
-import { RootState } from "../context";
-
 const HomePage = () => {
-  const {user} = useSelector((state: RootState) => state.auth)
-  console.log({user})
   return (
     <>
       <Typography variant="h3">Welcome ...</Typography>
