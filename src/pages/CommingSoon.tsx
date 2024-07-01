9e1de1e5af12997e0136da8d87ebd5f86b4387c9
YOUSSEF <imdevyoussef@gmail.com>
2024-07-01 22:36:06 +0300

    production: comming soon - typo update

--- src/pages/CommingSoon.tsx
+++ src/pages/CommingSoon.tsx
@@ -6,7 +6,7 @@ const CommingSoon = () => {
       <Box textAlign="center">
         <Typography variant="h5">Comming Soon</Typography>
         <Typography variant="body2">
-          The application is currently under construction, will be back soon.
+          The application is currently under construction, will be published soon.
         </Typography>
       </Box>
     </Container>
